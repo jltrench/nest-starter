1de0214b3618e6d46dfc86341de62cd9448b35f2
João Lucca Trench <jltrench@gmail.com>
2023-07-12 15:13:19 -0300

    simple crud example

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).send('Listagem de Cursos');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso #${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atualizacao do curso #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Curso deletado: #${id}`;
+  }
+}
